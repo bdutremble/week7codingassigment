5ac4a26b96e3b2d5f96c4edbaa1e194d98d1fd5d
Brian Dutremble <brian.dutremble@gmail.com>
2023-01-07 15:35:09 -0500

    finalish commit for first sql thing

---
+++ src/main/java/projects/Projects.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package projects;
+
+import projects.dao.DbConnection;
+
+/**
+ * @author briandutremble
+ *
+ */
+public class Projects {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbConnection.getConnection();
+
+	}
+
+}
