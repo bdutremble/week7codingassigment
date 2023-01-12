fb2ef55ddedbe18a7a22925a62a1e1bdf3b8a85e
Brian Dutremble <brian.dutremble@gmail.com>
2023-01-12 17:23:55 -0500

    added drawio diagram

--- src/main/resources/projects_schema.sql
+++ src/main/resources/projects_schema.sql
@@ -37,6 +37,9 @@ CREATE TABLE step (
 CREATE TABLE material (
     material_id INT AUTO_INCREMENT NOT NULL,
     project_id INT NOT NULL,
+    material_name VARCHAR(128) NOT NULL,
+    num_required INT,
+    cost DECIMAL(7,2),
     PRIMARY KEY (material_id),
     FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
